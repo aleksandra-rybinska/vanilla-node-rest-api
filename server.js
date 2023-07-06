469e25db1ac2e97a24aff64b38d8aa33a2f7394e
aleksandra rybinska <rybinska.aleksandra@gmail.com>
2023-07-06 11:31:11 +0200

    models and controlers

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const http = require('http')
-const products = require('./data/products.json')
+const http = require("http");
+const { getProducts } = require("./controllers/productController");
 
-const server = http.createServer((req, res)=> {
-    if(req.url === '/api/products' && req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify(products))
-    } else {
-        res.writeHead(404, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify({ message: "Route not found"}))
-    }
-})
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/products" && req.method === "GET") {
+    getProducts(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
