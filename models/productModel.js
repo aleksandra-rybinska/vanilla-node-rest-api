1df672e553c6162c814bd58067ad24bd47d09a03
aleksandra rybinska <rybinska.aleksandra@gmail.com>
2023-07-06 11:50:03 +0200

    getById request

--- models/productModel.js
+++ models/productModel.js
@@ -6,6 +6,14 @@ function findAll() {
   });
 }
 
+function findById(id) {
+  return new Promise((resolve, reject) => {
+    const product = products.find((item) => item.id === id);
+    resolve(product);
+  });
+}
+
 module.exports = {
   findAll,
+  findById,
 };
